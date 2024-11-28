d525a00ef9e17a94f80edaac49da66ed57c61d5d
Frederikke <frny0001@stud.kea.dk>
2024-11-28 17:32:36 +0100

    commit

---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+// Forside / Landingpage
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+
+        <div>
+       <h1 className="text-White">Hello World</h1>
+        </div>
+      </main>
+      <footer>
+      </footer>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,44 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'Red': '#BF2026',
+      'Black': '#303030',
+      'EggWhite': '#EDEDE9',
+      'White': '#FFFFFF',
+      'Grey': '#CECFD3',
+      'Navyblue': '#0A1C38',
+      'Darkblue': '#071324',
+    },
+
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+     /* animation: {
+        marquee: 'marquee 35s linear infinite',
+        //marquee2: 'marquee2 25s linear infinite',
       },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
+          //'100%': { transform: 'translateX(-100%)' },
+
+        },
+    
+        //marquee2: {
+         // '0%': { transform: 'translateX(100%)' },
+         // '100%': { transform: 'translateX(0%)' },
+       // },
+      },
+*/
+/*
+      fontFamily:{
+        'pText':['var(--outfit)', 'sans-serif'],
+      },
+      fontWeight:{
+        'pText': 400,
+      },
+*/
+   
     },
   },
   plugins: [],
