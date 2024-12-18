352f32e9572e02cbdc8e12bad0f2bef8603b5830
Frederikke <frny0001@stud.kea.dk>
2024-12-18 18:35:03 +0100

    ændring

--- src/app/components/Ad.jsx
+++ src/app/components/Ad.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Ad({ imageSrc, altText = "Annonce" }) {
   return (
-    <section className="relative w-full h-[100px] md:h-[200px] lg:h-[400px] flex justify-left">
+    <section className="relative w-full h-[125px] md:h-[200px] lg:h-[400px] flex justify-left">
       <h1 className="absolute z-10 text-xs lg:text-sm font-bold bg-black bg-opacity-50 text-white p-2">
         {altText}
       </h1>
